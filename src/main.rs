18910f689a871c986c8a4ac6aa609c611612ca4e
svl10 <seanvl123@gmail.com>
2025-10-19 12:46:26 +0000

    Added variable types

--- src/main.rs
+++ src/main.rs
@@ -5,55 +5,53 @@ fn main() {
     sys.refresh_all();
     let display_infos = DisplayInfo::all().unwrap();
 
-    let mut ram = Ram{
+    let mut ram = Ram {
         base_total_ram: sys.total_memory(),
         base_used_ram: sys.used_memory(),
         total_ram: sys.total_memory() as f64,
         used_ram: sys.used_memory() as f64,
         metric_prefix: String::from("bytes"),
-        };
+    };
 
     // Uncomment if you want to change prefix to mb or gb
     //ram.to_mb();
     ram.set_to_gb();
 
     // Set output distance
-    let distance = 5;
+    let distance: usize = 5;
 
-    let name = System::name().unwrap();
-    let host = System::host_name().unwrap();
-    let gpu = get_gpus()[0].clone();
-
+    let name: String = System::name().unwrap();
+    let host: String = System::host_name().unwrap();
+    let gpu: String = get_gpus()[0].clone();
 
     println!("System Name: {:>d$}", name, d = distance);
     println!("Host Name: {:>d$}", host, d = distance);
-    println!("Memory: {:>d$.2} {} / {:>d$.2} {}", ram.used_ram(), ram.metric_prefix, ram.total_ram(), ram.metric_prefix, d=distance);
+    println!(
+        "Memory: {:>d$.2} {} / {:>d$.2} {}",
+        ram.used_ram(),
+        ram.metric_prefix,
+        ram.total_ram(),
+        ram.metric_prefix,
+        d = distance
+    );
     println!("Processor: {}", sys.cpus()[0].brand());
     println!("GPUs: {}", gpu);
     println!("Monitor: {}", display_infos[0].friendly_name);
-    println!("Display: {} x {} @ {}hz", display_infos[0].width, display_infos[0].height, display_infos[0].frequency);
-
+    println!(
+        "Display: {} x {} @ {}hz",
+        display_infos[0].width, display_infos[0].height, display_infos[0].frequency
+    );
 
     println!("Time: {:?}", start.elapsed());
-
 }
 
-
-
-
-use sysinfo::{
-    System
-};
-use gfx_hal::{
-    adapter::Adapter,
-    Instance
-};
-use gfx_backend_vulkan as backend;
 use display_info::DisplayInfo;
+use gfx_backend_vulkan as backend;
+use gfx_hal::{adapter::Adapter, Instance};
 use std::time::Instant;
+use sysinfo::System;
 
 fn get_gpus() -> Vec<String> {
-
     let instance: gfx_backend_vulkan::Instance = backend::Instance::create("Graphics", 1).unwrap();
     let adapters: Vec<Adapter<backend::Backend>> = instance.enumerate_adapters();
 
@@ -66,7 +64,6 @@ fn get_gpus() -> Vec<String> {
     names
 }
 
-
 struct Ram {
     base_total_ram: u64,
     base_used_ram: u64,
@@ -76,7 +73,6 @@ struct Ram {
 }
 
 impl Ram {
-
     fn total_ram(&self) -> f64 {
         self.total_ram
     }
@@ -95,6 +91,4 @@ impl Ram {
         self.used_ram = self.base_used_ram as f64 / 1024.0 / 1024.0 / 1024.0;
         self.metric_prefix = String::from("GB");
     }
-
-
 }
