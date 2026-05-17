09322c4a00056261d1d307b448e69ed508ca4594
svl10 <seanvl123@gmail.com>
2026-05-17 21:15:13 +0800

    Added cat ascii arts and change output to a loop display

---
+++ src/ascii.rs
@@ -0,0 +1,38 @@
+
+
+pub fn cat_stretch() -> (Vec<String>, usize){
+    let thevec = vec![
+        r#"_._     _,-'""`-._"#.to_string(),
+        r#" (,-.`._,'(       |\`-/|"#.to_string(),
+        r#"     `-.-' \ )-`( , o o)"#.to_string(),
+        r#"           `-    \`_`"`-"#.to_string(),
+    ];
+    let width = thevec[3].len();
+    (thevec, width)
+}
+
+pub fn cat_sit() -> (Vec<String>, usize){
+    let thevec = vec![
+        r"  /\_/\  ".to_string(),
+        r" ( ^.^ ) ".to_string(),
+        r"  )   (  ".to_string(),
+        r" (     ) ".to_string(),
+        r"  \___// ".to_string()
+    ];
+
+    let width = thevec[1].len();
+    
+    (thevec, width)
+}
+
+pub fn cat_curious() -> (Vec<String>, usize){
+    let thevec = vec!{
+        r" /\_/\".to_string(),
+        r"( o.o )".to_string(),
+        r" > ^ <".to_string()
+    };
+
+    let width = thevec[1].len();
+    
+    (thevec, width)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::time::Instant;
 pub mod gpus;
 pub mod systeminfo;
 pub mod displayinfo;
-
+pub mod ascii;
 
 pub fn run() -> Result<(), Box<dyn Error>>{
     let start = Instant::now();
@@ -11,13 +11,36 @@ pub fn run() -> Result<(), Box<dyn Error>>{
     let gpu = gpus::get_gpus();
     let sysinfo = systeminfo::systeminfo();
     
-    println!("System Name: {}", sysinfo.sys_name());
-    println!("Host Name: {}", sysinfo.host_name());
-    println!("Fetch Time: {:?}", start.elapsed());
-    println!("{}", display_info);
-    println!("{}", monitor);
-    println!("CPU: {}", sysinfo.cpu_name());
-    println!("GPU: {}", gpu);
-    println!("Memory: {:.2} GiB / {:.2} GiB", sysinfo.used_ram(), sysinfo.total_ram());
+    let system_name = String::from(format!("System Name: {}", sysinfo.sys_name()));
+    let host_name = String::from(format!("Host Name: {}", sysinfo.host_name())); 
+    let fetch_time = String::from(format!("Fetch Time: {:?}", start.elapsed()));
+    let display_info = String::from(format!("{}", display_info));
+    let monitor = String::from(format!("{}", monitor));
+    let cpu = String::from(format!("CPU: {}", sysinfo.cpu_name()));
+    let gpu = String::from(format!("GPU: {}", gpu));
+    let memory = String::from(format!("Memory: {:.2} GiB / {:.2} GiB", sysinfo.used_ram(), sysinfo.total_ram()));
+    
+    let info = vec![
+        system_name,
+        host_name,
+        fetch_time,
+        display_info,
+        monitor,
+        cpu,
+        gpu,
+        memory
+    ];
+    let (ascii_art, mut width) = ascii::cat_sit();
+    width += 3;
+
+    for i in 0..ascii_art.len().max(info.len()){
+        let left_side = ascii_art.get(i).map_or("", |v| v);
+        let right_side = info.get(i).map_or("", |v| v);
+        
+        println!("{:<width$} | {}", left_side, right_side, width=width);
+    }
+
+
+
     Ok(())
 }
